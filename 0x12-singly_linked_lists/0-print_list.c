3b574d68f49152d55f1ba44de350cc7a1c4e3b84
ochuks1 <cochuks@gmail.com>
2023-10-20 16:36:00 -0700

    All answers to 0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,10 +2,10 @@
 #include "lists.h"
 
 /**
- * print_list - Prints all the elements of a list_t list.
- * @h: A pointer to the head of the list.
- * Return: The number of nodes.
- */
+* print_list - Prints all the elements of a list_t list.
+* @h: A pointer to the head of the list.
+* Return: The number of nodes.
+*/
 size_t print_list(const list_t *h)
 {
 size_t count = 0;
--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,10 +2,10 @@
 #include "lists.h"
 
 /**
- * list_len - Returns the number of elements in a list_t list.
- * @h: Pointer to the head of the list.
- * Return: The number of nodes in the list.
- */
+* list_len - Returns the number of elements in a list_t list.
+* @h: Pointer to the head of the list.
+* Return: The number of nodes in the list.
+*/
 size_t list_len(const list_t *h)
 {
 size_t count = 0;
