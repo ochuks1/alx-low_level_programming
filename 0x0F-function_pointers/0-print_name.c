59b1d736cbbff3721f8d50de1acdbffee47c0fb5
ochuks1 <cochuks@gmail.com>
2023-10-20 08:41:24 -0700

    All answers for 0x0F-function_pointers task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -14,3 +14,36 @@
 
 		f(name);
 	}
+
+/**
+ * print_name_as_is - Prints a name as is.
+ * @name: Name of the person.
+ */
+void print_name_as_is(char *name)
+{
+    printf("Hello, my name is %s\n", name);
+}
+
+/**
+ * print_name_uppercase - Prints a name in uppercase.
+ * @name: Name of the person.
+ */
+void print_name_uppercase(char *name)
+{
+    unsigned int i;
+
+    printf("Hello, my uppercase name is ");
+    i = 0;
+    while (name[i])
+    {
+        if (name[i] >= 'a' && name[i] <= 'z')
+        {
+            putchar(name[i] + 'A' - 'a');
+        }
+        else
+        {
+            putchar(name[i]);
+        }
+        i++;
+    }
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,6 +3,26 @@
 
 #include <stddef.h>
 
+/**
+ * struct op - Struct op
+ * @op: The operator
+ * @f: The function associated
+ */
+typedef struct op
+{
+    char *op; // The operator
+    int (*f)(int a, int b); // The function associated
+} op_t;
+
+/* Function prototypes */
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+
+
 /* Function prototype for printing a name */
 void print_name(char *name, void (*f)(char *));
 
