14d7def435736d029f49a1c37af12aee9641dbbd
ochuks1 <cochuks@gmail.com>
2023-10-20 08:54:00 -0700

    All answers for 0x0F-function_pointers task

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,19 +1,18 @@
 #include "function_pointers.h"
-	#include <stdio.h>
-	/**
-	 * print_name - print name using pointer to function
-	 * @name: string to add
-	 * @f: pointer to function
-	 * Return: nothing
-	 **/
-	void print_name(char *name, void (*f)(char *))
-	{
-		if (name == NULL || f == NULL)
-			return;
+#include <stdio.h>
+/**
+ * print_name - print name using pointer to function
+ * @name: string to add
+ * @f: pointer to function
+ * Return: nothing
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+if (name == NULL || f == NULL)
+return;
 
-
-		f(name);
-	}
+f(name);
+}
 
 /**
  * print_name_as_is - Prints a name as is.
@@ -22,28 +21,4 @@
 void print_name_as_is(char *name)
 {
     printf("Hello, my name is %s\n", name);
-}
-
-/**
- * print_name_uppercase - Prints a name in uppercase.
- * @name: Name of the person.
- */
-void print_name_uppercase(char *name)
-{
-    unsigned int i;
-
-    printf("Hello, my uppercase name is ");
-    i = 0;
-    while (name[i])
-    {
-        if (name[i] >= 'a' && name[i] <= 'z')
-        {
-            putchar(name[i] + 'A' - 'a');
-        }
-        else
-        {
-            putchar(name[i]);
-        }
-        i++;
-    }
 }
