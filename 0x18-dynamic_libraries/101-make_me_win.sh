72663e7da28daf022075cb9b5c0aeddee92fa7af
ochuks1 <cochuks@gmail.com>
2024-02-02 06:38:00 -0800

    101 make me win

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -2,5 +2,6 @@
 cp /bin/sh /tmp/win_sh
 chmod +x /tmp/win_sh
 export PATH="/tmp:$PATH"
-wget -P ..
+wget -P .. https://raw.github.com/ochuks1/alx-low_level_programming/master/0x18-dynamic_libraries/libgiga.so
 export LD_PRELOAD="PWD/../libgiga.so"
+python3 100-tests.py
