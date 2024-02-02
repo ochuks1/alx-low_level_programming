52bf5b184472f4a963c1fe977fb2959d7e678ad4
ochuks1 <cochuks@gmail.com>
2024-02-02 06:19:05 -0800

    Task 1

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 gcc -Wall -pedantic -Werror -Wextra -shared -fPIC *.c -o liball.so
-gcc *.c -c -fpic
+gcc -fPIC -c *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
