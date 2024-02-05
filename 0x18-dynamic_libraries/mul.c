2b2a558af25f4135ac60f863ab82fd83e121f516
ochuks1 <cochuks@gmail.com>
2024-02-04 19:43:57 -0800

    Task 2

---
+++ 0x18-dynamic_libraries/mul.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * mul - multiplication of two integers
+ * @a: first int
+ * @b: second int
+ * Return: multiply
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
